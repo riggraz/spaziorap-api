e768ab35faad276d0670cba7252a212a45d1bc0d
riccardo <riccardo.graziosi97@gmail.com>
2018-09-28 14:00:51 +0200

    Add deletion of posts with comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  has_many :children, class_name: 'Comment', foreign_key: 'parent_id'
+  has_many :children, class_name: 'Comment', foreign_key: 'parent_id', dependent: :destroy
   belongs_to :user
   belongs_to :post
   belongs_to :parent, class_name: 'Comment', foreign_key: 'parent_id', optional: true
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :topic
   has_many :likes, dependent: :destroy
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :body, presence: true
 end
