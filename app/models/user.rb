3af68ee44abfd43fddf61c6dece7a5e5eab3765c
riccardo <riccardo.graziosi97@gmail.com>
2018-10-12 17:23:50 +0200

    It is now possible to delete a User with notifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   has_many :received_notifications, class_name: 'Notification', foreign_key: 'receiver_id', dependent: :destroy
+  has_many :sent_notifications, class_name: 'Notification', foreign_key: 'sender_id', dependent: :destroy
 
   devise :database_authenticatable, :recoverable, :validatable
 
